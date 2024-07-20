ce6d36324f84f33bbb45f794f69b6b0e2966ed0b
Sion Lee <lso1030@handong.ac.kr>
2024-07-21 04:53:57 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 224 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -4,26 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        int[] times = new int[n];
+        int price_Y = 0;
+        int price_M = 0;
 
         for (int i = 0; i < n; i++) {
-            times[i] = s.nextInt();
+            int usage = s.nextInt();
+            price_Y  += (usage / 30 + 1) * 10;// Y 요금제: 30초마다 10원
+            price_M += (usage / 60 + 1) * 15; // M 요금제: 60초마다 15원
         }
 
-        int yCost = 0;
-        int mCost = 0;
-
-        for (int time : times) {
-            yCost += (time / 30 + 1) * 10;
-            mCost += (time / 60 + 1) * 15;
-        }
-
-        if (yCost < mCost) {
-            System.out.println("Y " + yCost);
-        } else if (mCost < yCost) {
-            System.out.println("M " + mCost);
-        } else {
-            System.out.println("Y M " + yCost);
-        }
+        if (price_Y < price_M)
+            System.out.print("Y " + price_Y);
+        else if (price_Y > price_M)
+            System.out.print("M " + price_M);
+        else
+            System.out.print("Y M " + price_Y);
     }
 }
