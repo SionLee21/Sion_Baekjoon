068a9a53157942d2d4e17a8526d197755250f98f
Sion Lee <lso1030@handong.ac.kr>
2024-07-21 17:16:24 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 240 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    Main ll=new Main();
-    ll.baekjoon();
-  }
-  void baekjoon(){
-    Scanner s=new Scanner(System.in);
-    int i,k,n;
-    int test=s.nextInt();
-    for (i=0; i<test; i++){ 
-      System.out.println(num(k=s.nextInt(),n=s.nextInt()));
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int T = s.nextInt(); // Test case 수
+
+        // 최대 k와 n의 값은 14
+        int[][] apt = new int[15][15];
+
+        // 0층 초기화: 0층 i호에는 i명이 산다
+        for (int i = 1; i <= 14; i++) {
+            apt[0][i] = i;
+        }
+
+        // 1층부터 14층까지 초기화
+        for (int k = 1; k <= 14; k++) {
+            for (int n = 1; n <= 14; n++) {
+                for (int j = 1; j <= n; j++) {
+                    apt[k][n] += apt[k-1][j];
+                }
+            }
+        }
+
+        // Test case별로 입력 받아 결과 출력
+        for (int t = 0; t < T; t++) {
+            int k = s.nextInt();
+            int n = s.nextInt();
+            System.out.println(apt[k][n]);
+        }
     }
-  }
-  int num(int k,int n){
-    if (k==0) return n;
-    if (n==0) return 0;
-    else return num(k,n-1)+num(k-1,n);
-  }
 }
